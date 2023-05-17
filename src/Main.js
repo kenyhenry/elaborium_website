bbd5258100ce01a8e0060746cc55e082c6e6c81f
kény Henry <henry.keny@outlook.fr>
2023-05-17 09:18:16 -0400

    add expand button

--- src/Main.js
+++ src/Main.js
@@ -1,63 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 import './App.css';
 import Header from './Header';
 import Navbar from './Navbar';
-import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const projects = [
   {
     title: 'Développement de scripts automatisés',
     description: 'Création de scripts personnalisés pour automatiser des tâches répétitives ou complexes.',
-    link: 'https://exemple.com/projet1',
+    text: '/Contactddd.smdfbDJFBNVLSDJKGHFVLSKDEJHFKASWDEBJVLKASDBVKJB',
   },
   {
     title: "Développement d'applications de bureau",
     description: "Création d'applications de bureau pour répondre aux besoins spécifiques d'une entreprise.",
-    link: 'https://exemple.com/projet2',
+    text: 'https://exemple.com/projet2',
   },
   {
     title: "Développement d'applications web",
     description: " Création d'applications web sur mesure, telles que des sites web interactifs ou des portails clients.",
-    link: 'https://exemple.com/projet3',
+    text: 'https://exemple.com/projet3',
   },
   {
     title: "Développement de logiciels personnalisés",
     description: " Création de logiciels sur mesure répondant aux besoins uniques d'une entreprise, avec une interface graphique conviviale.",
-    link: 'https://exemple.com/projet3',
+    text: 'https://exemple.com/projet3',
   },
   {
     title: 'Intégration de systèmes',
     description: "Intégration de différentes applications et systèmes existants pour améliorer l'efficacité et la productivité de l'entreprise.",
-    link: 'https://exemple.com/projet3',
+    text: 'https://exemple.com/projet3',
   },
   {
     title: 'Optimisation des performances',
     description: " Analyse et amélioration des performances des applications existantes pour assurer une utilisation plus efficace des ressources.",
-    link: 'https://exemple.com/projet3',
+    text: 'https://exemple.com/projet3',
   },
   {
     title: 'Sécurité des applications',
     description: " Audit et renforcement de la sécurité des applications et des systèmes pour protéger les données sensibles de l'entreprise.",
-    link: 'https://exemple.com/projet3',
+    text: 'https://exemple.com/projet3',
   },
   {
     title: 'Migration de données',
     description: "Assistance lors de la migration de données d'un système à un autre, en garantissant la cohérence et l'intégrité des données.",
-    link: 'https://exemple.com/projet3',
+    text: 'https://exemple.com/projet3',
   },
   {
     title: 'Formation et support technique',
     description: "Fourniture de formations et de support technique pour aider les utilisateurs à tirer le meilleur parti des applications développées.",
-    link: 'https://exemple.com/projet3',
+    text: 'https://exemple.com/projet3',
   },
   {
     title: "Développement d'applications mobiles (Indisponible)",
     description: "Création d'applications mobiles pour les plateformes iOS et Android, offrant des fonctionnalités spécifiques à l'entreprise.",
-    link: '',
+    text: '',
   },
 ];
 
 const Main = () => {
+  const initialState = Array.from({ length: projects.length }, () => false);
+  const [states, setStates] = useState(initialState);
+
   return (
     <div className="container">
       <Navbar></Navbar>
@@ -82,9 +86,19 @@ const Main = () => {
         <div className="project" key={index}>
           <h2 className="project-title">{project.title}</h2>
           <p className="project-description">{project.description}</p>
-          <a className="project-link" href={project.link} target="_blank" rel="noopener noreferrer">
-            Voir l'offre
-          </a>
+          {project.extended= states[index]}
+          <button onClick={() => {
+            setStates(prevStates => {
+              const newStates = [...prevStates];
+              newStates[index] = !newStates[index];
+              return newStates;
+            });
+          }}>
+            {project.extended ? 'Voir moins' : 'Voir plus'}
+          </button>
+          <p className="project-description">
+          {project.extended && ('\n') && (project.text)}
+          </p>
         </div>
       ))}
       <footer className="footer">
